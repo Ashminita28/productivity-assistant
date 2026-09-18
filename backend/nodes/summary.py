2694605b7d745f1c9f64eec9dc178dd1b68700ed
Ashminita <ashminitab@mindfiresolutions.com>
2026-09-18 12:40:47 +0530

    refactor(nodes): remove nodes from codebase because ReAct agent will be used

--- backend/nodes/summary.py
+++
@@ -1,6 +0,0 @@
-from backend.states.agent_state import AgentState
-from backend.tools.task_tools import execute_summarize_tasks
-
-def summarize_tasks_node(state: AgentState) -> dict:
-    result = execute_summarize_tasks()
-    return {"tool_result": result}
